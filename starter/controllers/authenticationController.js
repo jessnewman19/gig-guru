adb8f8c39f8cd40c722374c7667eeec0bd143c52
Jessica <jess.newman19@gmail.com>
2023-01-04 18:29:01 -0500

    implemented jsonwebtoken package to log in new users

--- starter/controllers/authenticationController.js
+++ starter/controllers/authenticationController.js
@@ -1,10 +1,21 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+//Login a new user that signs up
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirmation: req.body.passwordConfirmation,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'A user must have a username'],
   },
   email: {
     type: String,
